ecca16e36c5f1ff0861d82d42b64d112cf58d794
Moses <moses.junior@student.moringaschool.com>
2021-10-21 08:13:27 +0300

    displying data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {JsonPHService} from './services/json-ph.service'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+
+  data:Array<any>
+
+  constructor(private jsonplaceholder:JsonPHService) {
+    this.data = new Array<any>()
+  }
+
+  getDataFromAPI() {
+    this.jsonplaceholder.getData().subscribe((data) => {
+      console.log(data)
+      this.data = data
+    } )
+  }
 }
